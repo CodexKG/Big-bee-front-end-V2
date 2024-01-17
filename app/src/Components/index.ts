d77c0efe03b871266be6b5eb27a0334a766213b5
Timur <51020673+Timau32@users.noreply.github.com>
2024-01-18 00:22:00 +0600

    Revert "[C-0-18] add Promotion and PromotionCard"

--- app/src/Components/index.ts
+++ app/src/Components/index.ts
@@ -1,4 +1,2 @@
 import HeaderComponent from "./Header/Header"
-import Promotion from "./Promotion/Promotion"
-import PromotionCard from "./PromotionCard/PromotionCard"
-export { HeaderComponent ,Promotion,PromotionCard}
+export { HeaderComponent }
