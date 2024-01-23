fbb9b2b1e15473c8b7f422bc0126b4af1069fc1b
nazaraliev-kubanychbek <kuba.duishobaevich@gmail.com>
2024-01-23 16:57:57 +0600

    [C-0-19] delete images add carousel

--- app/src/Components/CategoryComponent/CategoryComponent.tsx
+++ app/src/Components/CategoryComponent/CategoryComponent.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import classes from './Category.module.scss';
 import moreIcon from './img/icon.svg';
-import img1 from './img/img1.png';
-import img2 from './img/img2.png';
-import img3 from './img/img3.png';
-import img4 from './img/img4.png';
-import CategoryCardComponent from '../CategoryCardComponent/CategoryCardComponent'
+import CategoryCardComponent from '../CategoryCardComponent/CategoryCardComponent';
+import { Carousel } from "antd";
+import { CaretLeftFilled, CaretRightFilled, } from "@ant-design/icons";
 
 const CategoryComponent: React.FC = () =>{
     const categories = [
@@ -13,25 +11,25 @@ const CategoryComponent: React.FC = () =>{
             id: 1,
             title: 'Наушники',
             price: 'от 14 990 с',
-            image: img1
+            image: 'https://gadgetstore.kz/wa-data/public/shop/products/42/03/342/images/1873/1873.970.jpeg'
         },
         {
             id: 2,
             title: 'Телефоны',
             price: 'от 94 990 с',
-            image: img2
+            image: 'https://www.myphone.kg/files/media/22/22499.png'
         },
         {
             id: 3,
             title: 'Ноутбуки',
             price: 'от 74 990 с',
-            image: img3
+            image: 'https://abi.kg/upload/iblock/fb3/ghpwpxfzumw64fazw1i0f9n72h8vrc9j.jpg   '
         },
         {
             id: 4,
             title: 'Фото и видео',
             price: 'от 74 990 с',
-            image: img4
+            image: 'https://s1.iconbird.com/ico/2013/7/391/w512h51213727793707dside.png'
         },
     ]
 
@@ -42,15 +40,46 @@ const CategoryComponent: React.FC = () =>{
                 <a href="#">Все товары <img src={moreIcon} alt="" /></a>
             </div>
 
-            <div className={classes.category_row}>
+            <Carousel
+              slidesToShow={3}
+              dots={false}
+              prevArrow={<ArrowLeft />}
+              nextArrow={<ArrowRight />}
+              slidesToScroll={1}
+              infinite={false}
+              arrows={true}
+            >
                 {
                     categories.map(item =>{
                         return <CategoryCardComponent key={item.id} item={item} />
                     })
                 }
-            </div>
+            </Carousel>
         </div>
     )
 }
 
 export default CategoryComponent;
+
+
+const ArrowLeft: React.FC<any> = ({ currentSlide, slideCount, ...props }) => (
+    <button
+      {...props}
+      className={`slick-prev slick-arrow ${classes.prevBtn}`}
+      style={{ display: currentSlide === 0 ? "none" : "block" }}
+    >
+      <CaretLeftFilled style={{ color: "black", fontSize: 15 }} />
+    </button>
+  );
+  const ArrowRight: React.FC<any> = ({ currentSlide, slideCount, ...props }) => {
+    return (
+      <button
+        {...props}
+        className={`slick-next slick-arrow ${classes.nextBtn}`}
+        style={{ display: currentSlide === 2 ? "none" : "block" }}
+        type="button"
+      >
+        <CaretRightFilled style={{ color: "black", fontSize: 15 }} />
+      </button>
+    );
+  };
