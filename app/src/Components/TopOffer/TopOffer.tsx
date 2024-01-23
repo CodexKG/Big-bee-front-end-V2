795a9f5e5c0dcc493a0c80363b7679bb6d19ac17
Sher06777 <asherbolot1@gmail.com>
2024-01-23 20:05:36 +0600

    [C-0-16] update TopOffer to change it height according to quantity of products

--- app/src/Components/TopOffer/TopOffer.tsx
+++ app/src/Components/TopOffer/TopOffer.tsx
@@ -1,47 +1,61 @@
 import React from 'react';
 import { Product } from 'types/types';
 import classes from './TopOffer.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { CaretRightOutlined } from "@ant-design/icons"
 
 interface TopOfferProps {
   products: Product[];
 }
 
+const getBlockSize = (quantityOfBlocks: number) => {
+  if (quantityOfBlocks === 4) {
+    return new Array(quantityOfBlocks).fill('50%');
+  }
+
+  if (quantityOfBlocks >= 1 && quantityOfBlocks <= 4) {
+    const baseSize = 92 / quantityOfBlocks;
+    const blockSizes = Array.from({ length: quantityOfBlocks }, (_, index) =>
+      index === quantityOfBlocks - 1 ? baseSize * 2 : baseSize
+    );
+
+    return blockSizes.map(size => `${size}%`);
+  }
+
+  return new Array(quantityOfBlocks).fill('100%');
+};
+
+const getDynamicClass = (quantityOfBlocks: number) => {
+  if (quantityOfBlocks >= 1 && quantityOfBlocks <= 4) {
+    return `x${quantityOfBlocks}`;
+  }
+
+  return 'default';
+};
+
 const TopOffer: React.FC<TopOfferProps> = ({ products }) => {
-  const getBlockSize = () => {
-    const quantityOfBlocks = products.length;
+  const quantityOfBlocks = products.length;
+  const blockSizes = getBlockSize(quantityOfBlocks);
+  const dynamicClass = getDynamicClass(quantityOfBlocks);
 
-    if (quantityOfBlocks === 4) {
-      return new Array(quantityOfBlocks).fill('50%');
-    }
-
-
-    if (quantityOfBlocks >= 1 && quantityOfBlocks <= 4) {
-      const baseSize = 92 / quantityOfBlocks;
-      const blockSizes = Array.from({ length: quantityOfBlocks }, (_, index) =>
-        index === quantityOfBlocks - 1 ? baseSize * 2 : baseSize
-      );
-
-      return blockSizes.map(size => `${size}%`);
-    }
-
-    return new Array(quantityOfBlocks).fill('100%');
-  };
-
-  const blockSizes = getBlockSize();
 
   return (
-    <div className={classes.topOffer}>
+    <div className={`${classes.topOffer} ${classes[dynamicClass]}`}>
       {products.map((product, index) => (
         <div key={index} style={{ flex: blockSizes[index] }} className={classes.topOffer_Block}>
-          <h3 className={classes.topOffer_Block_Title}>{product.title}</h3>
-          <p className={classes.topOffer_Block_Price}>{`Price: $${product.price.toFixed(2)} ${product.currency}`}</p>
-          <div className={classes.topOffer_Block_Image}>
-            <img src={product.image} alt={product.title} />
+          <div className={classes.topOffer_Block_Up}>
+            <h3 className={classes.topOffer_Block_Up_Title}>{product.title}</h3>
+            <p className={classes.topOffer_Block_Up_Price}>
+              {`Price: $${product.price.toFixed(2)} ${product.currency}`}
+            </p>
           </div>
-          <a className={classes.topOffer_Block_Link}>Перейти в каталог <FontAwesomeIcon icon={faCaretRight} />
-          </a>
+          <div className={classes.topOffer_Block_Down}>
+            <a className={classes.topOffer_Block_Down_Link}>
+              Перейти в каталог <CaretRightOutlined />
+            </a>
+            <div className={classes.topOffer_Block_Down_Image}>
+              <img src={product.image} alt={product.title} />
+            </div>
+          </div>
         </div>
       ))}
     </div>
