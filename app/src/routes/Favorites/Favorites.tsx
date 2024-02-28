31cfcf66a69037c400eb7f0c6b1a556c7ce58193
Bekbolot <justworking8888@gmail.com>
2024-02-28 23:45:29 +0600

    [main] add ci cd

--- app/src/routes/Favorites/Favorites.tsx
+++ app/src/routes/Favorites/Favorites.tsx
@@ -12,6 +12,7 @@ const Favorites: FC = () => {
     <Corset>
       <div className={classes.favorites}>
         {/* <Title level={1}>Избранное</Title> */}
+        helloƒ
         <Protected fallback={<NoLogin />}>
           <FavoritesProduct />
         </Protected>
