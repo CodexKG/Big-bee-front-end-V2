cab13416a7de94a18c82e17e7a9670f32a03b2aa
Bekbolot <justworking8888@gmail.com>
2024-03-11 20:14:32 +0600

    [main] remuve bugs

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,4 @@
 echo hello world 
-
 cd Bigbee-front
 ls
 git pull
@@ -8,5 +7,4 @@ npm run build
 sudo cp -rf ./build/* /var/www/bigbee.mnogosushi.kg/
 ls
 sudo systemctl restart nginx
-
 echo Successfully 
